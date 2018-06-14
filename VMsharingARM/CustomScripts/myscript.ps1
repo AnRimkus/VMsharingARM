72a25417e2d8de3c4f18de06585c586ce6a83737
Andrejs Rimsevics <an_rimkus@inbox.lv>
2018-06-14 17:17:41 +0300

    Run script downloaded using DSC by invocation from custom script extension.

--- VMsharingARM/CustomScripts/myscript.ps1
+++ VMsharingARM/CustomScripts/myscript.ps1
@@ -3,9 +3,11 @@ param (
 [string] $servicenamecs1
 ) 
 
-mkdir c:\$foldername
+#mkdir c:\$foldername
 mkdir c:\foldername2
 
-if ((Get-Service -Name Spooler).status -eq "running"){ 
-Stop-Service -Name Spooler
+if ((Get-Service -Name $servicenamecs1).status -eq "running"){ 
+Stop-Service -Name $servicenamecs1
 }
+
+powershell.exe -ExecutionPolicy Bypass -File c:\Scriptsara\outfile.ps1 $foldername $servicenamecs1
